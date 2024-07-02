d9d862a1b02b3f04c323e59b35563c295f726e8c
Mrblackant <anyapeng@bobfintech.com.cn>
2024-07-02 18:42:17 +0800

    好的能用，插入的位置待调整

--- extension.js
+++ extension.js
@@ -3,7 +3,8 @@ const fs = require("fs");
 const path = require("path");
 
 function activate(context) {
-  // 定义装饰器类型
+  let proxyConfig = {};
+
   const decorationType = vscode.window.createTextEditorDecorationType({
     after: {
       margin: "10px",
@@ -12,9 +13,6 @@ function activate(context) {
     },
   });
 
-  let proxyConfig = {};
-
-  // 读取并解析 vue.config.js 中的 proxy 配置
   function readProxyConfig() {
     const workspaceFolders = vscode.workspace.workspaceFolders;
     if (workspaceFolders) {
@@ -33,7 +31,18 @@ function activate(context) {
     }
   }
 
-  // 更新每项 proxy 配置的描述显示实际的请求地址
+  function getCompleteUrl(contextPath, proxyOptions) {
+    let completeUrl = proxyOptions.target || "";
+    if (proxyOptions.pathRewrite) {
+      for (let [key, value] of Object.entries(proxyOptions.pathRewrite)) {
+        if (contextPath.match(key)) {
+          contextPath = contextPath.replace(new RegExp(key), value);
+        }
+      }
+    }
+    return completeUrl + contextPath;
+  }
+
   function updateProxyDecorations() {
     const activeEditor = vscode.window.activeTextEditor;
     if (!activeEditor) {
@@ -42,34 +51,28 @@ function activate(context) {
 
     const document = activeEditor.document;
     const decorations = [];
-
+    // console.log("proxyConfig", proxyConfig);
     for (const contextPath in proxyConfig) {
       const proxyOptions = proxyConfig[contextPath];
-      let completeUrl = "";
-
-      if (typeof proxyOptions === "string") {
-        completeUrl = proxyOptions;
-      } else if (typeof proxyOptions === "object" && proxyOptions.target) {
-        completeUrl = proxyOptions.target;
-      }
+      const completeUrl = getCompleteUrl(contextPath, proxyOptions);
+      // console.log("proxyOptions", proxyOptions);
+      console.log("completeUrl", contextPath);
 
       if (completeUrl) {
-        const contextPaths = Array.isArray(contextPath)
-          ? contextPath
-          : [contextPath];
-
         for (let i = 0; i < document.lineCount; i++) {
           const line = document.lineAt(i);
-          if (line.text.includes("target")) {
-            const targetPosition = line.range.end;
-            contextPaths.forEach((contextPathItem) => {
-              const completeUrlText = ` Complete URL: ${completeUrl}${contextPathItem}`;
-              const decoration = {
-                range: new vscode.Range(targetPosition, targetPosition),
-                renderOptions: { after: { contentText: completeUrlText } },
-              };
-              decorations.push(decoration);
-            });
+          // console.log("line", line.text);
+          let ttt = line.text;
+          if (line.text.includes("target") && line.text.includes(contextPath)) {
+            const targetIndex = line.text.indexOf("target");
+            const targetPosition = new vscode.Position(i, targetIndex);
+
+            const completeUrlText = `实际请求地址: ${completeUrl}`;
+            const decoration = {
+              range: new vscode.Range(targetPosition, targetPosition),
+              renderOptions: { after: { contentText: completeUrlText } },
+            };
+            decorations.push(decoration);
             break;
           }
         }
@@ -79,7 +82,6 @@ function activate(context) {
     activeEditor.setDecorations(decorationType, decorations);
   }
 
-  // 监听 vue.config.js 文件的变化
   const watcher = vscode.workspace.createFileSystemWatcher("**/vue.config.js");
   watcher.onDidChange(readProxyConfig);
   watcher.onDidCreate(readProxyConfig);
@@ -88,7 +90,6 @@ function activate(context) {
     updateProxyDecorations();
   });
 
-  // 注册文档打开事件监听
   vscode.workspace.onDidOpenTextDocument((document) => {
     if (
       document.languageId === "javascript" ||
@@ -101,7 +102,6 @@ function activate(context) {
     }
   });
 
-  // 注册文本编辑器的保存事件监听
   vscode.workspace.onDidSaveTextDocument((document) => {
     if (
       document.languageId === "javascript" ||
@@ -114,7 +114,6 @@ function activate(context) {
     }
   });
 
-  // 注册文本编辑器的激活事件监听
   vscode.window.onDidChangeActiveTextEditor((editor) => {
     if (editor) {
       updateProxyDecorations();
@@ -124,7 +123,6 @@ function activate(context) {
   context.subscriptions.push(watcher);
   context.subscriptions.push(decorationType);
 
-  // 初始化时立即展示完整的请求地址
   readProxyConfig();
 }
 
