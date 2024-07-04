35dca99895890e721ce1416662f430e35b8f42c6
Mrblackant <anyapeng@bobfintech.com.cn>
2024-07-04 12:00:46 +0800

    待优化快速console已实现

--- extension.js
+++ extension.js
@@ -1,43 +1,54 @@
 const vscode = require("vscode");
 
+/**
+ * @param {vscode.ExtensionContext} context
+ */
 function activate(context) {
-  console.log(
-    'Congratulations, your extension "console-log-snippet" is now active!'
-  );
-
-  // 注册代码片段提供程序
-  context.subscriptions.push(
-    vscode.languages.registerCompletionItemProvider("*", {
+  let disposable = vscode.languages.registerCompletionItemProvider(
+    { scheme: "file", language: "javascript" },
+    {
       provideCompletionItems(document, position, token, context) {
-        // 获取当前行的文本
         const linePrefix = document
           .lineAt(position)
           .text.substr(0, position.character);
-        // 检查当前行的文本，包括在当前位置之前的字符
-        if (linePrefix.includes("clg")) {
-          // 创建完成项目
-          const completionItem = new vscode.CompletionItem(
-            "console.log()",
-            vscode.CompletionItemKind.Method
-          );
-          completionItem.insertText = new vscode.SnippetString(
-            "console.log($1);$0"
-          );
-          completionItem.documentation = new vscode.MarkdownString(
-            "Inserts console.log() statement with cursor inside parentheses"
-          );
-          completionItem.range = new vscode.Range(
-            position.translate(0, -3),
-            position
-          ); // 设置光标位置
-          return [completionItem];
+        if (linePrefix.endsWith("clg")) {
+          return [
+            new vscode.CompletionItem(
+              "console.log()",
+              vscode.CompletionItemKind.Snippet
+            ),
+          ];
+        } else if (linePrefix.endsWith("clgj")) {
+          return [
+            new vscode.CompletionItem(
+              "console.log(JSON.parse(JSON.stringify()))",
+              vscode.CompletionItemKind.Snippet
+            ),
+          ];
         }
         return [];
       },
-    })
+    }
   );
+
+  context.subscriptions.push(disposable);
+
+  // 监听用户的输入，按回车时触发补全
+  vscode.workspace.onDidChangeTextDocument((event) => {
+    const { document, contentChanges } = event;
+    const lastChange = contentChanges[contentChanges.length - 1];
+    const text = lastChange.text.trim();
+    const range = lastChange.range;
+
+    if (text === "clg" || text === "clgj") {
+      vscode.commands.executeCommand("editor.action.triggerSuggest");
+    }
+  });
 }
+
+function deactivate() {}
 
 module.exports = {
   activate,
+  deactivate,
 };
